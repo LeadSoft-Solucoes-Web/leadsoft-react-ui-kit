4d20400bfd5e45ae970a52b357aa485b81f39577
guidolinzinho <lucas.guedes@leadsoft.inf.br>
2024-10-22 18:48:30 -0300

    subindo alterações

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as SelectLanguage } from "./SelectLanguage/SelectLanguage";
 export { default as ColorPicker } from "./ColorPicker/ColorPicker";
+export { default as ImageCrop } from "./ImageCrop/ImageCrop";
