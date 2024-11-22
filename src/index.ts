bc468de744637ab185caa057a619ee3d8be9acbb
guidolinho <lucas.guedes@leadsoft.inf.br>
2024-11-22 10:50:54 -0300

    subindo novo componente de radio button

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as SelectLanguage } from "./SelectLanguage/SelectLanguage";
 export { default as ColorPicker } from "./ColorPicker/ColorPicker";
 export { default as ImageCrop } from "./ImageCrop/ImageCrop";
 export { default as ImageUplader } from "./Image/Image";
+export { default as RadioButton } from "./RadioButton/RadioButton";
