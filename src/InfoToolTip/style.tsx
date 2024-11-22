a4bacc4c1a4b22ed83443200ecb3d66f7f1f5593
guidolinho <lucas.guedes@leadsoft.inf.br>
2024-11-22 13:57:11 -0300

    subindo novo componente de info tool tip

---
+++ src/InfoToolTip/style.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { ColorsType } from "./types";
+
+export const Container = styled.div<ColorsType>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 20px;
+    height: 20px;
+    border: 1px solid ${(props) => (props.borderColor ? props.borderColor : 'blue')};
+    color: ${(props) => (props.borderColor ? props.borderColor : 'blue')};
+    border-radius: 40px;
+    position: relative; 
+
+    &::after {
+        content: attr(data-tooltip); 
+        position: absolute;
+        bottom: -40px; 
+        left: 50%;
+        transform: translateX(-50%);
+        background: #333;
+        color: #fff;
+        padding: 5px 10px;
+        border-radius: 5px;
+        font-size: 12px;
+        white-space: nowrap;
+        opacity: 0;
+        pointer-events: none;
+        transition: opacity 0.3s, bottom 0.3s;
+        z-index: 1000;
+    }
+
+    &:hover::after {
+        opacity: 1;
+    }
+
+    @media (max-height: 60px) {
+        &::after {
+            bottom: auto;
+            top: -40px; 
+        }
+    }
+`;
